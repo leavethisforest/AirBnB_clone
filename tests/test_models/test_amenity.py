30f2e5b04e176b24eec907fa8e1bf5568eeeba7e
leavethisforest <126917567+leavethisforest@users.noreply.github.com>
2023-10-15 08:39:46 +0200

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -151,9 +151,9 @@ class TestAmenity_to_dict(unittest.TestCase):
 
     def test_to_dict_contains_added_attributes(self):
         am = Amenity()
-        am.middle_name = "Holberton"
+        am.middle_name = "BestSchool"
         am.my_number = 98
-        self.assertEqual("Holberton", am.middle_name)
+        self.assertEqual("BestSchool", am.middle_name)
         self.assertIn("my_number", am.to_dict())
 
     def test_to_dict_datetime_attributes_are_strs(self):
